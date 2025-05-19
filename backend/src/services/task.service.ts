cb380c5dbf9ae17e6661cebde5bd0d568e4e1e4a
Wuelber Castillo <wuelber.castillo@gmail.com>
2025-05-19 13:40:09 -0600

    Implement task shifting

--- backend/src/services/task.service.ts
+++ backend/src/services/task.service.ts
@@ -48,30 +48,18 @@ class TaskService {
       oldTask.position !== task.position &&
       oldTask.category.toString() !== task.category
     ) {
-      const tasks = await Task.find({
-        position: { $gte: task.position },
-        category: task.category,
-      });
-
-      for (const task of tasks) {
-        task.position += 1;
-        await task.save();
-      }
+      await this.shiftTasksInNewCategory(task.position, task.category);
+      await this.reorderTaskInOldCategory(
+        oldTask.position,
+        oldTask.category.toString()
+      );
     }
 
     if (
       oldTask.position !== task.position &&
       oldTask.category.toString() === task.category
     ) {
-      const tasks = await Task.find({
-        position: { $gte: task.position },
-        category: oldTask.category.toString(),
-      });
-
-      for (const task of tasks) {
-        task.position += 1;
-        await task.save();
-      }
+      await this.shiftTasksInSameCategory(oldTask.position, task.position);
     }
 
     return Task.findByIdAndUpdate(id, task, { new: true });
@@ -79,6 +67,66 @@ class TaskService {
 
   async deleteTask(id: string) {
     return Task.findByIdAndDelete(id);
+  }
+
+  private async shiftTasksInSameCategory(
+    oldPosition: number,
+    newPosition: number
+  ) {
+    const movedUp = oldPosition < newPosition;
+    let tasks = [];
+
+    if (movedUp) {
+      tasks = await Task.find({
+        position: { $gte: oldPosition, $lte: newPosition },
+      });
+      for (const task of tasks) {
+        task.position -= 1;
+        await task.save();
+      }
+      return;
+    }
+
+    tasks = await Task.find({
+      position: { $gte: newPosition, $lte: oldPosition },
+    });
+
+    for (const task of tasks) {
+      task.position += 1;
+      await task.save();
+    }
+  }
+
+  private async shiftTasksInNewCategory(
+    newPosition: number,
+    newCategoryId: string
+  ) {
+    const tasks = await Task.find({
+      position: { $gte: newPosition },
+      category: newCategoryId,
+    });
+
+    for (const task of tasks) {
+      task.position += 1;
+      await task.save();
+    }
+    return;
+  }
+
+  private async reorderTaskInOldCategory(
+    oldPosition: number,
+    oldCategoryId: string
+  ) {
+    const tasks = await Task.find({
+      position: { $gte: oldPosition },
+      category: oldCategoryId,
+    });
+
+    for (const task of tasks) {
+      task.position -= 1;
+      await task.save();
+    }
+    return;
   }
 }
 
